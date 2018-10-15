e0384b3fabb6953959afa7c212740a803479febc
Yalamarthi Rajesh <rajeshchowdary3230@gmail.com>
2018-10-15 18:48:20 +0530

    15_oct_2018

---
+++ SharePointCSOMAssessment/SharePointCSOMAssessment/ErrorWriteToLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace SharePointCSOMAssessment
+{
+    class ErrorWriteToLog
+    {
+        static public void WriteToLogFile(Exception e)
+        {
+            string ErrorString = "-- " + DateTime.Now + " : " + e.StackTrace + " : "+ e.Message + Environment.NewLine + Environment.NewLine + Environment.NewLine;
+            string FilePath = @"D:\logs1.txt";
+            
+           // Console.WriteLine("Exists :" + File.Exists(FilePath));
+            File.AppendAllText(FilePath, ErrorString);
+        }
+    }
+}
